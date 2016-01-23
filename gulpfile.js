48492744eb7e80eb8bc8c91c98a17525252ffc2e
Robbie <exrobbie@gmail.com>
2016-01-23 11:00:35 +0800

    BBBBBB

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp=require("gulp");
+var uglify=require("gulp-uglify");
+gulp.task('uglify',function(){
+	return gulp.src('js/**/main.js').pipe(uglify()).pipe(gulp.dest('build'));
+})
--- js/main.js
+++
@@ -1,4 +0,0 @@
-//set the height of content-top
-var aWidth=window.screen.availWidth;
-var aHeight=window.screen.availHeight;
-document.getElementById("content-top").style.height=aHeight+"px";
