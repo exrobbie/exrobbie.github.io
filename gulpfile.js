1787b7bbea9e5eea3af358c66d2938d21e52426e
litianao <litianao@le.com>
2016-04-07 18:34:43 +0800

    Adding gulp-autoprefixer support

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
-var gulp=require('gulp');
-var uglify=require('gulp-uglify');
-var jshint=require('gulp-jshint');
-var less=require('gulp-less');
-gulp.task('js',function(){
-	return gulp.src('./src/js/main.js')
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	.pipe(uglify())
-	.pipe(gulp.dest('./dest/js/'));
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+gulp.task('js', function() {
+    return gulp.src('./src/js/main.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dest/js/'));
 });
 
-gulp.task('css',function(){
-	return gulp.src('./src/css/*.less')
-	.pipe(less())
-	.pipe(gulp.dest('./dest/css/'));
-})
+gulp.task('css', function() {
+    return gulp.src('./src/css/*.less')
+        .pipe(less())
+		.pipe(autoprefixer({
+			browsers: ['firefox < 20','ie 9'],
+			cascade: false
+		}))
+        .pipe(gulp.dest('./dest/css/'));
+});
+
+gulp.task('default', ['js', 'css']);
